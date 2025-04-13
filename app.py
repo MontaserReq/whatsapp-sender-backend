76ddd15eabb7b9a06c933a1aa196ea96e974f36f
montaser <montaserreq@gmail.com>
2025-04-13 20:04:44 +0300

    change

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def send_whatsapp():
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
